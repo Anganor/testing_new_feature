ec998119d5dc32b996d8e512c7a953d1ef98ef94
Anganor <anganor@gmail.com>
2019-09-14 10:47:19 +0200

    Submodule 7.2

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -1,16 +1,10 @@
 package com.kodilla.stream;
 
-import com.kodilla.stream.beautifier.PoemBeautifier;
+import com.kodilla.stream.iterate.NumbersGenerator;
 
 public class StreamMain {
     public static void main(String[] args) {
-        PoemBeautifier poemBeautifier = new PoemBeautifier();
-        poemBeautifier.beautify("A", "", (one, two) -> "Battery type: " + one + one);
-        poemBeautifier.beautify("triple", "A", (one, two) -> "Battery type: " + one + "-" + two);
-        poemBeautifier.beautify("A", "b", (one, two) -> one.toLowerCase() + two.toUpperCase());
-        poemBeautifier.beautify("!!!", "This sentence needed an adequate exclamation", (one, two) -> two + one);
-        poemBeautifier.beautify("A", "B", (one, two) -> "So we can change one with two: " + one.replace('A', 'B'));
-        poemBeautifier.beautify("A", "B", (one, two) -> "The two strings match: " + two.matches(one));
-        poemBeautifier.beautify("Ala ma kota.", "A teraz juz nie ma ", (one, two) -> one + " " + two + one.substring(7));
+        System.out.println("Using Stream to generate even numbers from 1 to 20");
+        NumbersGenerator.generateEven(20);
     }
 }
