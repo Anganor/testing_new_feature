e7b96a703877f3bb97296cc86f397095e4fc4407
Artur Kicia <akicia@keywordsstudios.com>
2019-09-16 10:35:52 +0200

    Submodule 7.4

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -6,7 +6,6 @@ import com.kodilla.stream.forumuser.ForumUser;
 import java.time.LocalDate;
 import java.time.Period;
 import java.util.Map;
-import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
 public class StreamMain {
@@ -22,5 +21,7 @@ public class StreamMain {
         filteredForumUsers.entrySet().stream()
                 .map(entry -> entry.getKey() + ": " + entry.getValue())
                 .forEach(System.out::println);
+
+        System.out.println("Test git/gradle");
     }
 }
