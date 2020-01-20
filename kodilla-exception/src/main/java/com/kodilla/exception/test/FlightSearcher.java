c2ea107f3a98c121e840554c79d2e2774740ed6b
Anganor <anganor@gmail.com>
2020-01-20 13:15:38 +0100

    Submodule 8.4

--- kodilla-exception/src/main/java/com/kodilla/exception/test/FlightSearcher.java
+++ kodilla-exception/src/main/java/com/kodilla/exception/test/FlightSearcher.java
@@ -1,14 +1,29 @@
 package com.kodilla.exception.test;
 
-import java.util.*;
+import java.util.HashMap;
+import java.util.Map;
 
 public class FlightSearcher {
-    public Map<String, Boolean> findFlight(Flight flight) {
-        HashMap<String, Boolean> availableFlights = new HashMap<>();
-        availableFlights.put("Katowice", true);
-        availableFlights.put("Kraków", true);
-        availableFlights.put("Warszawa", true);
-        availableFlights.put("Wrocław", false);
-        return availableFlights;
+    public void findFlight(Flight flight) throws RouteNotFoundException {
+        Map<String, Boolean> availableAirports = new HashMap<>();
+        availableAirports.put("Poznań", true);
+        availableAirports.put("Kraków", true);
+        availableAirports.put("Warszawa", true);
+        availableAirports.put("Wrocław", true);
+        availableAirports.put("Katowice", true);
+        availableAirports.put("Gdańsk", true);
+        availableAirports.put("Gdynia", true);
+
+        if (!availableAirports.containsKey(flight.getDepartureAirport()) && !availableAirports.containsKey(flight.getArrivalAirport())) {
+            throw new RouteNotFoundException("Both chosen airports are not on our map.");
+        }
+
+        if (!availableAirports.containsKey(flight.getArrivalAirport())) {
+            throw new RouteNotFoundException("Your destination airport: " + flight.getArrivalAirport() + ", is not on our map.");
+        }
+
+        if (!availableAirports.containsKey(flight.getDepartureAirport())) {
+            throw new RouteNotFoundException("Your starting airport: " + flight.getDepartureAirport() + ", is not on our map.");
+        }
     }
 }
--- kodilla-exception/src/main/java/com/kodilla/exception/test/FlightSearcherRunner.java
+++ kodilla-exception/src/main/java/com/kodilla/exception/test/FlightSearcherRunner.java
@@ -5,7 +5,52 @@ public class FlightSearcherRunner {
         Flight flight1 = new Flight("Katowice", "Warszawa");
         Flight flight2 = new Flight("Katowice", "Kraków");
         Flight flight3 = new Flight("Kraków", "Wrocław");
+        Flight flight4 = new Flight("Sosnowiec", "Katowice");
+        Flight flight5 = new Flight("Gdańsk", "Malbork");
+        Flight flight6 = new Flight("Białystok", "Szczecin");
 
-        
+        FlightSearcher flightSearcher = new FlightSearcher();
+
+        try {
+            flightSearcher.findFlight(flight1);
+            System.out.println("Flight from: " + flight1.getDepartureAirport() + ", to: " + flight1.getArrivalAirport() + " is available.");
+        } catch (RouteNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            flightSearcher.findFlight(flight2);
+            System.out.println("Flight from: " + flight2.getDepartureAirport() + ", to: " + flight2.getArrivalAirport() + " is available.");
+        } catch (RouteNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            flightSearcher.findFlight(flight3);
+            System.out.println("Flight from: " + flight3.getDepartureAirport() + ", to: " + flight3.getArrivalAirport() + " is available.");
+        } catch (RouteNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            flightSearcher.findFlight(flight4);
+            System.out.println("Flight from: " + flight4.getDepartureAirport() + ", to: " + flight4.getArrivalAirport() + " is available.");
+        } catch (RouteNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            flightSearcher.findFlight(flight5);
+            System.out.println("Flight from: " + flight5.getDepartureAirport() + ", to: " + flight5.getArrivalAirport() + " is available.");
+        } catch (RouteNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            flightSearcher.findFlight(flight6);
+            System.out.println("Flight from: " + flight6.getDepartureAirport() + ", to: " + flight6.getArrivalAirport() + " is available.");
+        } catch (RouteNotFoundException e) {
+            e.printStackTrace();
+        }
     }
 }
