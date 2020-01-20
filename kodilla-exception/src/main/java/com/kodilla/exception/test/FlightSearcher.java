35a7cac77cb480f32e17f0ffdd720927a5e461ee
Anganor <anganor@gmail.com>
2020-01-20 21:02:24 +0100

    Submodule 8.4 addendum

--- kodilla-exception/src/main/java/com/kodilla/exception/test/FlightSearcher.java
+++ kodilla-exception/src/main/java/com/kodilla/exception/test/FlightSearcher.java
@@ -1,5 +1,6 @@
 package com.kodilla.exception.test;
 
+import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -8,12 +9,19 @@ public class FlightSearcher {
         Map<String, Boolean> availableAirports = new HashMap<>();
         availableAirports.put("Poznań", true);
         availableAirports.put("Kraków", true);
-        availableAirports.put("Warszawa", true);
-        availableAirports.put("Wrocław", true);
+        availableAirports.put("Warszawa", false);
+        availableAirports.put("Wrocław", false);
         availableAirports.put("Katowice", true);
         availableAirports.put("Gdańsk", true);
         availableAirports.put("Gdynia", true);
 
+        if (availableAirports.containsKey(flight.arrivalAirport) && availableAirports.containsKey(flight.departureAirport)) {
+            if (!availableAirports.get(flight.arrivalAirport)) {
+                System.out.println("Chosen airport: " + flight.arrivalAirport + ", is currently under maintenance.");
+            } else if (!availableAirports.get(flight.departureAirport)) {
+                System.out.println("Chosen airport: " + flight.departureAirport + ", is currently under maintenance.");
+            }
+        }
         if (!availableAirports.containsKey(flight.getDepartureAirport()) && !availableAirports.containsKey(flight.getArrivalAirport())) {
             throw new RouteNotFoundException("Both chosen airports are not on our map.");
         }
