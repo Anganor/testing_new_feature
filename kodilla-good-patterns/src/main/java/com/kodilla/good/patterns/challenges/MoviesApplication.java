8738d9327b84e6fa50ecdd6f5cc39429f65157fc
Anganor <anganor@gmail.com>
2020-02-13 13:37:29 +0100

    Submodule 13.1

---
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/MoviesApplication.java
@@ -0,0 +1,20 @@
+package com.kodilla.good.patterns.challenges;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class MoviesApplication {
+    public static void main(String[] args) {
+        MovieStore movieStore = new MovieStore();
+        Map<String, List<String>> movieMap = movieStore.getMovies();
+
+        String movieTitles = movieMap.entrySet().stream()
+                .map(m -> m.getValue())
+                .flatMap(m -> m.stream())
+                .collect(Collectors.joining(" ! ", "", ""));
+
+        System.out.println(movieTitles);
+    }
+}
