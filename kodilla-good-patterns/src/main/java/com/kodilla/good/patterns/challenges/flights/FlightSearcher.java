ed958bbce20b7cecad4b2e05f5e7d3592c9c291a
Anganor <anganor@gmail.com>
2020-02-23 10:45:41 +0100

    Submodule 13.5 addendum

--- kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/flights/FlightSearcher.java
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/flights/FlightSearcher.java
@@ -1,28 +1,31 @@
 package com.kodilla.good.patterns.challenges.flights;
 
+import java.util.HashSet;
 import java.util.Set;
 import java.util.stream.Collectors;
 
 public class FlightSearcher {
     public void flightsTo(Set<Flight> flightsTo, String arrivalAirport) {
-        System.out.println("Available connections to " + arrivalAirport + ":");
+        System.out.println("Available connections to " + arrivalAirport + " airport:");
         flightsTo.stream()
                 .filter(f -> f.getArrivalAirport().equals(arrivalAirport))
                 .forEach(System.out::println);
     }
 
     public void flightsFrom(Set<Flight> flightsFrom, String departureAirport) {
-        System.out.println("Available connections from " + departureAirport + ":");
+        System.out.println("Available connections from " + departureAirport + " airport:");
         flightsFrom.stream()
                 .filter(f -> f.getDepartureAirport().equals(departureAirport))
                 .forEach(System.out::println);
     }
 
-    public void flightsVia(Set<Flight> flightsVia, String departureAirport, String arrivalAirport) {
-        Set<Flight> flightsListFrom = flightsVia.stream()
+    public void flightsVia(String departureAirport, String arrivalAirport) {
+        FlightRepository flightRepository = new FlightRepository();
+        Set<Flight> flightsList = flightRepository.getListOfFlights();
+        Set<Flight> flightsListFrom = flightsList.stream()
                 .filter(f -> f.getDepartureAirport().equals(departureAirport))
                 .collect(Collectors.toSet());
-        Set<Flight> flightsListTo = flightsVia.stream()
+        Set<Flight> flightsListTo = flightsList.stream()
                 .filter(f -> f.getArrivalAirport().equals(arrivalAirport))
                 .collect(Collectors.toSet());
         for(Flight flightFrom : flightsListFrom){
